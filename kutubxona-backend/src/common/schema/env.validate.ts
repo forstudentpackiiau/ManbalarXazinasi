72f93c41a9091bbe50afc9557ed7018e0bb2a774
nurillo <nkozimovv@gmail.com>
2025-05-20 18:24:27 +0500

    feat: schema support Render DATABASE_URL

--- kutubxona-backend/src/common/schema/env.validate.ts
+++ kutubxona-backend/src/common/schema/env.validate.ts
@@ -1,13 +1,39 @@
 import * as Joi from "joi";
+
 export const serverSchema = Joi.object({
-    PORT: Joi.number().required(),
-    POSTGRES_USER: Joi.string().required(),
-    POSTGRES_DB: Joi.string().required(),
-    POSTGRES_HOST: Joi.string().required(),
-    POSTGRES_PASSWORD: Joi.string().required(),
-    POSTGRES_PORT: Joi.number().required(),
-    KUTUBXONACHI_LOGINI: Joi.string().required(),
-    KUTUBXONACHI_PAROLI: Joi.string().required(),
-    USTOZ_LOGINI: Joi.string().required(),
-    USTOZ_PAROLI: Joi.string().required(),
-  });
+  PORT: Joi.number().required(),
+
+  // Accept either DATABASE_URL or all individual DB variables
+  DATABASE_URL: Joi.string().uri().optional(),
+
+  POSTGRES_USER: Joi.alternatives().conditional('DATABASE_URL', {
+    is: Joi.exist(),
+    then: Joi.optional(),
+    otherwise: Joi.string().required()
+  }),
+  POSTGRES_DB: Joi.alternatives().conditional('DATABASE_URL', {
+    is: Joi.exist(),
+    then: Joi.optional(),
+    otherwise: Joi.string().required()
+  }),
+  POSTGRES_HOST: Joi.alternatives().conditional('DATABASE_URL', {
+    is: Joi.exist(),
+    then: Joi.optional(),
+    otherwise: Joi.string().required()
+  }),
+  POSTGRES_PASSWORD: Joi.alternatives().conditional('DATABASE_URL', {
+    is: Joi.exist(),
+    then: Joi.optional(),
+    otherwise: Joi.string().required()
+  }),
+  POSTGRES_PORT: Joi.alternatives().conditional('DATABASE_URL', {
+    is: Joi.exist(),
+    then: Joi.optional(),
+    otherwise: Joi.number().required()
+  }),
+
+  KUTUBXONACHI_LOGINI: Joi.string().required(),
+  KUTUBXONACHI_PAROLI: Joi.string().required(),
+  USTOZ_LOGINI: Joi.string().required(),
+  USTOZ_PAROLI: Joi.string().required(),
+});
