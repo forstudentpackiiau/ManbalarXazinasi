db7165bf86b51b6f990668ec494e7ab2628e0420
nurillo <nkozimovv@gmail.com>
2025-05-20 18:56:15 +0500

    fix: correct Joi schema for dual DB config

--- kutubxona-backend/src/kutubxona.module.ts
+++ kutubxona-backend/src/kutubxona.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { KitoblarModule } from './modules/kitoblar/kitoblar.module';
-import { databaseProviders } from './common/base/database.providers';
+import { DatabaseService } from './common/base/database.providers';
 import { KategoriyaModule } from './modules/kategoriya/kategoriya.module';
 import { LoginModule } from './modules/login/login.module';
 import { join } from 'node:path';
@@ -11,6 +11,6 @@ import { ServeStaticModule } from '@nestjs/serve-static';
   imports: [ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'uploads'), // Static fayllar uchun yo‘l
     serveRoot: '/uploads', // URL orqali kirish yo‘li
-  }), databaseProviders, KitoblarModule, KategoriyaModule, LoginModule],
+  }), DatabaseService, KitoblarModule, KategoriyaModule, LoginModule],
 })
 export class KutubxonaBackendModule {}
